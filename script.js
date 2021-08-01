c9613b06f9fb0a482d490dde7fb62fa0084b46ab
Aitolkun Salymbaeva <salymbaeva17@gmail.com>
2021-08-01 23:32:03 +0600

    html&scss done

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const r1 = document.querySelector("#r1")
 const offsetX = document.querySelector("#offsetX")
 const offsetY = document.querySelector("#offsetY")
-const spread = document.querySelector("#spread")
 const blur = document.querySelector("#blur")
+const spread = document.querySelector("#spread")
 const color = document.querySelector("#color")
 const boxBgColor = document.querySelector("#bg-color")
 const box = document.querySelector(".box")
@@ -26,15 +26,15 @@ function generateBoxBackgroundColor (){
 generateBoxBackgroundColor()
 
 function generateBoxShadow(){
-    let type = ""
+    let type
     if (inset.checked){
         type = "inset"
     } else{
         type = ""
     }
     shadowResult.innerHTML = `box-shadow: ${offsetX.value}px ${offsetY.value}px ${blur.value}px ${spread.value}px ${color.value} ${type};`
-    shadowResult.innerHTML = shadowResult.innerHTML +  `<br/> -webkit-box-shadow: ${offsetX.value}px ${offsetY.value}px ${blur.value}px ${spread.value}px ${color.value} ${type};`
-    shadowResult.innerHTML = shadowResult.innerHTML +  `<br/> -moz-box-shadow: ${offsetX.value}px ${offsetY.value}px ${blur.value}px ${spread.value}px ${color.value} ${type};`
+    shadowResult.innerHTML = shadowResult.innerHTML +  `<br> -webkit-box-shadow: ${offsetX.value}px ${offsetY.value}px ${blur.value}px ${spread.value}px ${color.value} ${type};`
+    shadowResult.innerHTML = shadowResult.innerHTML +  `<br> -moz-box-shadow: ${offsetX.value}px ${offsetY.value}px ${blur.value}px ${spread.value}px ${color.value} ${type};`
     box.style.borderRadius = r1.value + "%"
     box.style.boxShadow = `${offsetX.value}px ${offsetY.value}px ${blur.value}px ${spread.value}px ${color.value} ${type}`
 }
