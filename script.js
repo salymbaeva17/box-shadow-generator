1be3f1eec4063dd43d90956baf78c3163ead06c4
Aitolkun Salymbaeva <salymbaeva17@gmail.com>
2021-08-02 08:45:42 +0600

    settings values

--- script.js
+++ script.js
@@ -9,6 +9,12 @@ const box = document.querySelector(".generate__box")
 const shadowResult = document.querySelector(".generate__shadow-result")
 const inset = document.querySelector("#inset")
 const outset = document.querySelector("#outset")
+const borderRadius = document.querySelector(".r1-value")
+const blurRadius = document.querySelector(".blur-radius")
+const spreadRadius = document.querySelector(".spread-radius")
+const hLength = document.querySelector(".h-length")
+const vLength = document.querySelector(".v-length")
+
 // r1.onchange = generateBoxShadow
 r1.oninput = generateBoxShadow
 offsetX.oninput = generateBoxShadow
@@ -19,6 +25,11 @@ color.oninput = generateBoxShadow
 boxBgColor.oninput = generateBoxBackgroundColor
 inset.oninput = generateBoxShadow
 outset.oninput = generateBoxShadow
+borderRadius.oninput = generateBoxShadow
+hLength.oninput = generateBoxShadow
+vLength.oninput = generateBoxShadow
+blurRadius.oninput = generateBoxShadow
+spreadRadius.oninput = generateBoxShadow
 
 function generateBoxBackgroundColor () {
     box.style.backgroundColor = boxBgColor.value
@@ -32,6 +43,11 @@ function generateBoxShadow(){
     } else{
         type = ""
     }
+    blurRadius.textContent = blur.value
+   spreadRadius.textContent = spread.value
+    hLength.textContent = offsetX.value
+    vLength.textContent = offsetY.value
+    borderRadius.textContent = r1.value
     shadowResult.innerHTML = `box-shadow: ${offsetX.value}px ${offsetY.value}px ${blur.value}px ${spread.value}px ${color.value} ${type};`
     shadowResult.innerHTML = shadowResult.innerHTML +  `<br> -webkit-box-shadow: ${offsetX.value}px ${offsetY.value}px ${blur.value}px ${spread.value}px ${color.value} ${type};`
     shadowResult.innerHTML = shadowResult.innerHTML +  `<br> -moz-box-shadow: ${offsetX.value}px ${offsetY.value}px ${blur.value}px ${spread.value}px ${color.value} ${type};`
